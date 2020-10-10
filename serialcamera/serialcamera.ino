2de054010f353898860b75e43936c54c66829d36
Jarod Lam <jlam96@gmail.com>
2020-10-10 14:05:54 +1000

    Working camera over serial

---
+++ serialcamera/serialcamera.ino
@@ -0,0 +1,63 @@
+#include "esp_camera.h"
+
+#define CAMERA_MODEL_AI_THINKER
+#include "camera_pins.h"
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  
+  // Image format options
+  // Frame size options: QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+  config.frame_size = FRAMESIZE_VGA; 
+  config.jpeg_quality = 10;
+  config.fb_count = 2;
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+}
+
+void loop() {
+  camera_fb_t *fb = esp_camera_fb_get();
+  const char *fbData = (const char *)fb->buf;
+  size_t fbLen = fb->len;
+
+  Serial.print("imageSize:");
+  Serial.println(fbLen);
+  Serial.println("START JPEG IMAGE");
+  for (int i = 0; i < fbLen; i++) {
+    Serial.print(fbData[i]);
+  }
+  Serial.println("END JPEG IMAGE");
+  Serial.println();
+
+  esp_camera_fb_return(fb);
+
+  delay(1000);
+}
